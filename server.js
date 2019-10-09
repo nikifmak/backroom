bb92b00b8f032d780f4d5d856c314fa399f74f0b
nikifmak <nikifmak@gmail.com>
2019-10-09 19:52:38 +0300

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const { sequelize } = require("./models");
 
@@ -18,11 +19,7 @@ sequelize
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
