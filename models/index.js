1945a18c1e7275e077cd7fa228dc1385c090a042
nikifmak <nikifmak@gmail.com>
2019-10-28 21:19:34 +0200

    changed config to env based

--- models/index.js
+++ models/index.js
@@ -4,21 +4,17 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+let sequelize = new Sequelize(
+  process.env.RDS_DATABASE,
+  process.env.RDS_USERNAME,
+  process.env.RDS_PASSWORD,
+  {
+    host: process.env.RDS_HOSTNAME,
+    dialect: "postgres"
+  }
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
